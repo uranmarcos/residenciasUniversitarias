ef74a64363a05493fb1829637eb5d7e512d329db
Marcos Uran <marcos_uran@hotmail.com>
2022-01-09 13:52:35 -0300

    visualizacion de pedido en formato pdf

--- pedidos2.php
+++ pedidos2.php
@@ -4,20 +4,17 @@ require("funciones/pdo.php");
     $alertErrorConexion = "hide";
     $alertConfirmacion = "hide";
     $mensajeAlertConfirmacion="";
-
-    // CONSULTAS INICIALES LISTADO DE ARTICULOS, MEDIDAS Y CATEGORIAS
-    $consultaPedidos = $baseDeDatos ->prepare("SELECT PN.id, PN.fecha, A.nombre, A.segundoNombre, A.apellido FROM pedidosnuevos PN INNER JOIN
-     agentes A ON PN.usuario = A.id ORDER BY PN.fecha DESC");
-    //$consultaSedes = $baseDeDatos ->prepare("SELECT * FROM sedes WHERE habilitado = '1'");
     
+    // CONSULTAS DE TODOS LOS PEDIDOS
+    $consultaPedidos = $baseDeDatos ->prepare("SELECT PN.id, PN.sede, PN.fecha, A.nombre, A.segundoNombre, A.apellido FROM pedidosnuevos PN INNER JOIN
+     agentes A ON PN.usuario = A.id ORDER BY PN.fecha DESC");    
     try {
         $consultaPedidos->execute();
-        //$consultaSedes->execute();
     } catch (\Throwable $th) {
         $alertErrorConexion= "show";
     }
     $pedidos = $consultaPedidos -> fetchAll(PDO::FETCH_ASSOC);
-    //$sedes = $consultaSedes -> fetchAll(PDO::FETCH_ASSOC);
+
     $noHayDatos = "show";
     $hayDatos = "hide";
     if(sizeof($pedidos) != 0) {
@@ -25,7 +22,82 @@ require("funciones/pdo.php");
         $hayDatos = "show";
     }
  
- 
+    if(isset($_POST["verPedido"])){
+        $id = $_POST["id"];
+        //consulto a base de datos el pedido clickeado para ver
+        $pedidoConsultado = $baseDeDatos ->prepare("SELECT PN.pedido, PN.sede, PN.fecha, PN.casa, A.nombre, A.segundoNombre, A.apellido FROM pedidosnuevos PN INNER JOIN
+        agentes A ON PN.usuario = A.id WHERE PN.id = $id");
+        try {
+            $pedidoConsultado->execute();
+        } catch (\Throwable $th) {
+            $alertErrorConexion= "show";
+        }
+        $pedido = $pedidoConsultado -> fetchAll(PDO::FETCH_ASSOC);
+        $idSede = $pedido[0]["sede"];
+
+
+        //consulta de la sede del pedido
+        $consultaSede = $baseDeDatos ->prepare("SELECT * from sedes WHERE id = '$idSede'");
+        try {
+            $consultaSede->execute();
+        } catch (\Throwable $th) {
+            $alertErrorConexion= "show";
+        }
+        $sede = $consultaSede -> fetchAll(PDO::FETCH_ASSOC);
+        // conversion de formato de fecha
+        $newDate = date("d-m-Y", strtotime($pedido[0]["fecha"]));
+        $otrosTabla = "";
+        $pedidoTabla = [];
+
+        // consulta listado de articulos
+        $consultaArticulos = $baseDeDatos ->prepare("SELECT A.id, A.descripcion, A.medida, M.descripcion medida from articulos A INNER JOIN medidas M WHERE A.medida = M.id");
+        $consultaArticulosValidada = false;
+        try {
+            $consultaArticulos->execute();
+            $consultaArticulosValidada = true;
+        } catch (\Throwable $th) {
+            $alertErrorConexion = "show";
+        }
+        if ($consultaArticulosValidada) {
+            $articulos = $consultaArticulos -> fetchAll(PDO::FETCH_ASSOC);
+            $pedidoAMostrar = explode(";", $pedido[0]["pedido"]);
+            foreach($pedidoAMostrar as $p){
+                $item = explode(":", $p);
+                if($item[0] != "otros" && $item[0] != "" ){
+                    $key = array_search($item[0], array_column($articulos, "id"));
+                    $nuevoItem = [];
+                    array_push($nuevoItem, $articulos[$key]["descripcion"] . ": " . $item[1] . " " . $articulos[$key]["medida"]); 
+                    array_push($pedidoTabla, $nuevoItem);
+                }
+                if($item[0] == "otros"){
+                    $otrosTabla = $item[1];
+                }
+            }
+            sort($pedidoTabla);
+            require("armarPdf.php");
+        try {
+            $pdf = new PDF();
+            $pdf->AliasNbPages();
+            $header = array('Listado de articulos pedidos');
+            $pdf->AddPage();
+            $pdf->SetFont('Arial','B',12);
+            $pdf->Cell(0,5,"Pedido Mensual de " . utf8_decode($sede[0]["descripcion"]),0,1,'C');
+            $pdf->Cell(0,10,'Fecha: ' . $newDate,0,1);
+            $pdf->Cell(0,10,'Casa: ' . $pedido[0]["casa"] . " -  Voluntario: " . $pedido[0]["nombre"] . " " . $pedido[0]["segundoNombre"] . " " . $pedido[0]["apellido"],0,1);
+            $pdf->SetFont('Arial','B',12);
+            $pdf->Cell(0,10,'Articulos pedidos: ' ,0,1);
+            $pdf->SetFont('Arial','',12);
+            $pdf->TablaSimple($header,$pedidoTabla);
+            $pdf->SetFont('Arial','B',12);
+            $pdf->Cell(0,10,'Otros: ' ,0,1);
+            $pdf->SetFont('Arial','',12);
+            $pdf->Cell(190,50,utf8_decode($otrosTabla),1);
+            $pdf->Output();
+        } catch (\Throwable $th) {
+            $alertErrorConexion= "show";
+        }
+        }
+    }
 
 
 ?>
@@ -80,13 +152,14 @@ require("funciones/pdo.php");
                                 </tr>
                             </thead>
                             <tbody>
-                                    <?php foreach($pedidos as $pedido){ ?>
+                                <?php foreach($pedidos as $pedido){ ?>
+                                    <form method="POST" target="_blank" action="pedidos2.php">
                                         <tr>
-                                            <td><?php echo $pedido["id"] ?></td>
+                                            <td><input type="text" style ="width:50px; border: none" name="id" readonly value="<?php echo $pedido["id"] ?>"></td>
                                             <td><?php echo $pedido["fecha"]?></td>
                                             <td><?php echo $pedido["nombre"] . " " . $pedido["segundoNombre"] . " " . $pedido["apellido"] ?></td>
                                             <td class="d-flex justify-content-start"> 
-                                                <button type="button" class="btn editButton" onclick="abrirPedido()" data-bs-toggle="modal" data-bs-target="#modalEliminar">
+                                                <button type="submit" class="btn editButton" name="verPedido"  data-bs-toggle="modal" data-bs-target="#modalEliminar">
                                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye-fill" viewBox="0 0 16 16">
                                                         <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z"/>
                                                         <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z"/>
@@ -94,7 +167,8 @@ require("funciones/pdo.php");
                                                 </button>
                                             </td>
                                         </tr>
-                                    <?php } ?>   
+                                    </form>
+                                <?php } ?>   
                             </tbody>               
                         </table>
                     </div>
