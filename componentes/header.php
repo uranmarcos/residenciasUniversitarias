b8c5293c28a33ef6d0785abf5d7150a909a00529
Marcos Uran <marcos_uran@hotmail.com>
2022-01-31 20:15:52 -0300

    correccion menu responsive

--- componentes/header.php
+++ componentes/header.php
@@ -4,7 +4,7 @@ if($_SESSION["autenticado"] != true){
 }
 $mostrarBotonAdmin = "hide";
 if($_SESSION["rol"] == "admin" || $_SESSION["rol"] == "general"){
-    $mostrarBotonAdmin = "show";
+    $mostrarBotonAdmin = "";
 }
 
 ?>
@@ -68,15 +68,15 @@ if($_SESSION["rol"] == "admin" || $_SESSION["rol"] == "general"){
                 </svg>
             </button>
             <button name="perfil" class="row asideButton" id="btnPerfil" data-toggle="tooltip" data-placement="bottom" onclick="activarBoton('perfil', 'perfilSeccion')">
-                <div class=" nameButtonAside">Perfil</div> 
+                <div class="nameButtonAside">Perfil</div> 
                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="icono bi bi-person-fill" viewBox="0 0 16 16">
                     <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
                 </svg>
             </button>
             <button name="admin" class="row asideButton <?php echo $mostrarBotonAdmin?>" onclick="resetStorage()" id="admin" data-toggle="tooltip" data-placement="bottom" onclick="activarBoton('admin', 'adminSeccion')">
-                <div class="nameButtonAside">Admin</div>
+                <div class="nameButtonAside botonAdmin">Admin</div>
                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="icono bi bi-gear-fill" viewBox="0 0 16 16">
-                <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z"/>
+                    <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z"/>
                 </svg>
             </button>
             <button name="pedidos" class="row asideButton" id="pedidos" data-toggle="tooltip" data-placement="bottom" onclick="activarBoton('listadoPedidos', 'listadoPedidosSeccion')">
@@ -86,7 +86,7 @@ if($_SESSION["rol"] == "admin" || $_SESSION["rol"] == "general"){
                 </svg>
             </button>
             <button name="ayuda" class="row asideButton <?php echo $mostrarBotonAdmin?>" id="ayuda" data-toggle="tooltip" data-placement="bottom" onclick="activarBoton('admin', 'adminSeccion')">
-                <div class="nameButtonAside">Ayuda</div>
+                <div class="nameButtonAside botonAdmin">Ayuda</div>
                 <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="icono bi bi-question-square-fill" viewBox="0 0 16 16">
                     <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm3.496 6.033a.237.237 0 0 1-.24-.247C5.35 4.091 6.737 3.5 8.005 3.5c1.396 0 2.672.73 2.672 2.24 0 1.08-.635 1.594-1.244 2.057-.737.559-1.01.768-1.01 1.486v.105a.25.25 0 0 1-.25.25h-.81a.25.25 0 0 1-.25-.246l-.004-.217c-.038-.927.495-1.498 1.168-1.987.59-.444.965-.736.965-1.371 0-.825-.628-1.168-1.314-1.168-.803 0-1.253.478-1.342 1.134-.018.137-.128.25-.266.25h-.825zm2.325 6.443c-.584 0-1.009-.394-1.009-.927 0-.552.425-.94 1.01-.94.609 0 1.028.388 1.028.94 0 .533-.42.927-1.029.927z"/>
                 </svg>
